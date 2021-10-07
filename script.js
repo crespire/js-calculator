4b580c68e0d247317de8325f228d29c4cbe45de2
crespire <li.simmon@gmail.com>
2021-10-07 15:27:31 -0400

    Add basic JavaScript and update HTML to use number IDs

---
+++ script.js
@@ -0,0 +1,27 @@
+function doAdd(x, y) {
+    return x + y;
+}
+
+function doSubtract(x, y) {
+    return x - y;
+}
+
+function doMultiply(x, y) {
+    return x * y;
+}
+
+function doDivide(x, y) {
+    return x / y;
+}
+
+function doCalc(...queue) {
+
+}
+
+function handleInput(event) {
+    console.dir(event);
+}
+
+let currentQueue = [];
+const inputsDiv = document.querySelector("#inputs");
+inputsDiv.addEventListener('mouseclick', handleInput);
