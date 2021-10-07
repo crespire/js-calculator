0be6513f608e00bfe726ece3ae0871713c67e924
crespire <li.simmon@gmail.com>
2021-10-07 16:03:16 -0400

    Adds functionality to handleInput() event handler.

--- script.js
+++ script.js
@@ -15,13 +15,40 @@ function doDivide(x, y) {
 }
 
 function doCalc(...queue) {
+    
+}
 
+function updateDisplay(newLine, ...queue) {
+    /**
+     * 
+     */
 }
 
 function handleInput(event) {
-    console.dir(event);
+
+    /**
+     * Grab the event target's ID as a new token
+     *   this is either a number, operation, or command.
+     * 
+     * If command is
+     *   equals and queue is full, then run the operation and return the result.
+     *   clear, then clear the queue
+     * else
+     *   add token to queue
+     * 
+     * Update Display
+     */
+    let newToken = event.target.id;
+    let needNewLine = false;
+
+    if (currentQueue.length = 3 && newToken === "equals") {
+       doCalc(currentQueue);
+    }
+
+    updateDisplay(needNewLine, currentQueue);
 }
 
 let currentQueue = [];
-const inputsDiv = document.querySelector("#inputs");
+const displayDiv = document.querySelector('#display');
+const inputsDiv = document.querySelector('#inputs');
 inputsDiv.addEventListener('click', handleInput);
